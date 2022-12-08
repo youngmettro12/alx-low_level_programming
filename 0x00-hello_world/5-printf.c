9521138fc36e41b4a8ed632f4c2ddc441ee52bf2
ian murigu <ianomurigu12@gmail.com>
2022-12-08 11:01:59 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 /**
+ * main - print a c program using printf
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: 0
+*/
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 }

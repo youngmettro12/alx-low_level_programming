cba0581c655a419f412c8e60244f72216304046d
ian murigu <ianomurigu12@gmail.com>
2022-12-08 09:37:26 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
+ * main - Entry
  *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
+ * Return: Always 0 (success) 
+ */
 
 int main(void)
 
@@ -15,5 +13,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-
 }

526e3c83414036ce53f956fd783618b33ec5eb40
ian murigu <ianomurigu12@gmail.com>
2022-12-08 12:25:33 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main -print a line of code using puts
+ * main - Entry Point
  *
- * Return: 0
-*/
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{
-puts("\"Programing is like building a multilingual puzzle");
-return (0);
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * main - print out sizes of data types in C
+ * code by bwave ict
+ * Return: 0
+*/
 int main(void)
-
 {
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
-
+        
+	printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu bytes(S)\n", (unsigned long)sizeof(c));
+	printf("Size of long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
+	printf("Size of float: %lu byte(S)\n", (unsigned long)sizeof(f));
 	return (0);
-
 }

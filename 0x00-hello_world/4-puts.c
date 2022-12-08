1a459c642a845a4641cea43e04a0880c36c6a642
ian murigu <ianomurigu12@gmail.com>
2022-12-08 09:49:30 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,10 @@
 
 /**
  * main - Entry
- *
  * Return: Always 0 (success) 
  */
-
 int main(void)
-
-{
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+{	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

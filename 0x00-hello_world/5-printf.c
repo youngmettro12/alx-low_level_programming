2d41dfef5b618f860c2dcf797be85e44fdcd04a5
ian murigu <ianomurigu12@gmail.com>
2022-12-08 08:55:16 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,23 @@
-#include <stdio.h>
+i#include <stdio.h>
+
+
 
 /**
  *
- *  * Description: main - Prints out a string to stdout.
+ *  * main - Entry point
  *
- *   * Return: 0 if success.
+ *   *
  *
- *    */
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return (0);
+			return (0);
 
 }

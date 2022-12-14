7529a02eaf6cf6e73311e8568c8f2dc8384e3dc1
ian murigu <ianomurigu12@gmail.com>
2022-12-14 02:35:50 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Retrun: 0
+ */
+
+void print_alphabet(void)
+
+{
+	char C = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+
+}
